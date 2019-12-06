08b130c6b60fcfd11185fad4e82b4504fc94b5ee
Cheng Chungtheng <cheng.chungtheng@sabay.com>
2019-12-06 15:36:47 +0700

    [init]

---
+++ src/App.svelte
@@ -0,0 +1,12 @@
+
+
+<script>
+	import SUM from "./sum.svelte"
+	let src = '';
+	let name = 'Rick Astley';
+</script>
+<input type="text" bind:value={src}>
+
+<img {src} alt="{name} dances.">
+<br/>
+<SUM/>
